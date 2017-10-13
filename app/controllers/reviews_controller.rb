37b8d8c45d6a303aba2d500a4ce89400f6837ec8
David Grant <mrdavidgrant@outlook.com>
2017-10-13 14:01:58 -0400

    posts submitting

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,23 @@
+class ReviewsController < ApplicationController
+
+  def create
+    product = Product.find params[:product_id] 
+    @user = logged_in?
+    @review = product.reviews.new(review_params)
+    @review.user_id = @user.id
+    
+    if @review.save
+      redirect_to product
+    else
+      redirect_to product
+    end
+
+  end
+
+  private
+
+    def review_params
+      params.require(:review).permit(:description, :rating)
+    end
+
+end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :user   
   belongs_to :product
 
-  validates :product_id, presence: true
-  validates :user_id, presence: true
+
   validates :rating, presence: true
 
 
